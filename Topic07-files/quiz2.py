4e4edc9300a2e77d8e4438c0f733f7d4b47efc4d
Sophia Gotch <sophiegotch@users.noreply.github.com>
2025-08-06 20:17:16 +0100

    Fix final logic for run count

--- Topic07-files/quiz2.py
+++ Topic07-files/quiz2.py
@@ -1,22 +1,3 @@
-# quiz2.py
-
-FILENAME = "Topic07-files/count.txt"
-
-def readNumber():
-    with open(FILENAME) as f:
-        number = int(f.read())
-    return number
-
-def writeNumber(number):
-    with open(FILENAME, "wt") as f:
-        f.write(str(number))
-
-# Test the functions
-num = readNumber()
-print("Current number in file:", num)
-
-writeNumber(3)
-print("Wrote number 3 to file.")
 
 # quiz2.py
 
@@ -31,9 +12,7 @@ def writeNumber(number):
     with open(FILENAME, "wt") as f:
         f.write(str(number))
 
-# Main program
 num = readNumber()
+num += 3
 print(f"This program has been run {num} times.")
-
-num += 1
 writeNumber(num)
