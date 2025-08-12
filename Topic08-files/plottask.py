d1b4758dd500e65636d82addae3a1b9d1f934438
Sophia Gotch <sophiegotch@users.noreply.github.com>
2025-08-12 21:56:47 +0100

    Add Lab 3.2 programs

--- Topic08-files/plottask.py
+++ Topic08-files/plottask.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# data
+# Generate 1000 values from a normal distribution
 mean = 5
 std_dev = 2
 values = np.random.normal(mean, std_dev, 1000)
 
-# function h(x) = x^2 on [0,10]
+# x for h(x)=x^2, from 0 to 10
 x = np.linspace(0, 10, 100)
 y = x**2
 
-# plot (same axes)
+# Plot on the same axes
 plt.figure(figsize=(10, 6))
 plt.hist(values, bins=30, alpha=0.6, label='Normal Distribution', edgecolor='black')
 plt.plot(x, y, linewidth=2, label='h(x) = x²')
@@ -20,6 +20,5 @@ plt.title('Histogram of Normal Distribution & Function h(x) = x²')
 plt.grid(True, linestyle='--', alpha=0.6)
 plt.legend()
 
-# save PNG next to this script
-plt.savefig('plot.png', dpi=300)
+plt.savefig('Topic08-files/plot.png', dpi=300)
 plt.show()
